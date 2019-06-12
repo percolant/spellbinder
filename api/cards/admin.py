e2325e735e3db4b38412fc6edb728a00207094ef
Andrej Snitko <snitko@seznam.cz>
2019-06-13 01:16:23 +0200

    customized Django admin page for Card model

--- api/cards/admin.py
+++ api/cards/admin.py
@@ -11,10 +11,23 @@ from cards.models import (
 )
 
 
+class CardAdmin(admin.ModelAdmin):
+    list_display=('base', 'type_line', 'mana_cost', 'rarity', 'power_toughness', )
+    list_filter=('edition', 'rarity', 'colors', 'formats', )
+
+    def base(self, obj):
+        return obj.__str__()
+
+    def power_toughness(self, obj):
+        if obj.power and obj.toughness:
+            return '{}/{}'.format(obj.power, obj.toughness)
+        return ''
+
+
 admin.site.register(Edition)
 admin.site.register(Color)
 admin.site.register(Format)
 admin.site.register(Artist)
 admin.site.register(Rarity)
-admin.site.register(Card)
+admin.site.register(Card, CardAdmin)
 admin.site.register(CardInstance)
